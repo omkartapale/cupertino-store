1ad7bc549782ccfb9a72a84c898ba7020a8642bd
Omkar Tapale <omkartapale@gmail.com>
2023-09-05 20:44:34 +0530

    Completed Step 7 Add product search

--- lib/search_tab.dart
+++ lib/search_tab.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import 'model/app_state_model.dart';
+import 'product_row_item.dart';
+import 'search_bar.dart';
+import 'styles.dart';
 
 class SearchTab extends StatefulWidget {
   const SearchTab({super.key});
@@ -10,14 +16,73 @@ class SearchTab extends StatefulWidget {
 }
 
 class _SearchTabState extends State<SearchTab> {
+  late final TextEditingController _controller;
+  late final FocusNode _focusNode;
+  String _terms = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController()..addListener(_onTextChanged);
+    _focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onTextChanged() {
+    setState(() {
+      _terms = _controller.text;
+    });
+  }
+
+  Widget _buildSearchBox() {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: SearchBar(
+        controller: _controller,
+        focusNode: _focusNode,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: <Widget>[
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('Search'),
+    final model = Provider.of<AppStateModel>(context);
+    final results = model.search(_terms);
+
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        color: Styles.scaffoldBackground,
+      ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            _buildSearchBox(),
+            Expanded(
+              child: SingleChildScrollView(
+                // Todo: Known Bug - Fails if searched result list is empty
+                // CupertinoListSection Failed assertion: line 215 pos 15:
+                // '(children != null && children.length > 0) || header !=
+                // null': is not true.)
+                child: CupertinoListSection(
+                  topMargin: 0,
+                  children: [
+                    for (var product in results)
+                      ProductRowItem(
+                        product: product,
+                      )
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
